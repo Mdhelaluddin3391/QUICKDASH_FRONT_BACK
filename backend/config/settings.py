62752c0c3782126597c98a24c3bcb5e5e15092dc
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2026-01-08 23:21:46 +0530

    some updates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,8 @@ else:
             "http://127.0.0.1:5000",
             "https://quickdash.com",
             "https://www.quickdash.com",
+            "http://localhost:8080",
+            "http://127.0.0.1:8080",
         ]
 
 CORS_ALLOW_CREDENTIALS = True
