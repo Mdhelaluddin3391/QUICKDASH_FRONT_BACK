7032eb0d10f57c3b3fd7dfb3dca384ea262ea75c
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2026-01-25 22:27:23 +0530

    some updates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = [
     "storages",
     "django_celery_beat",
     'leaflet',
-    'apps.orders.apps.OrdersConfig',
+    # 'apps.orders.apps.OrdersConfig',
 
     # Local Apps
     "apps.accounts",
