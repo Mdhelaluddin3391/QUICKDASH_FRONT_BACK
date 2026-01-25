a709a7aa5363e84f9cf35ea67a9475d03132c789
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2026-01-25 12:50:25 +0530

    some updates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -322,3 +322,13 @@ LEAFLET_CONFIG = {
     'SCALE': 'both',
     'ATTRIBUTION_PREFIX': 'QuickDash',
 }
+
+
+# ------------------------------------------------------------------------------
+# CSRF CONFIGURATION
+# ------------------------------------------------------------------------------
+# This tells Django to trust requests coming from your localhost
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    "http://localhost:8000,http://127.0.0.1:8000"
+).split(",")
