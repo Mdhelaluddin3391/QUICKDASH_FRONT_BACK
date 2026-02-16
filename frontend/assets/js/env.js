173a32a4dd44868d14381df2db5284c76d37570b
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2026-02-16 23:21:27 +0530

    some updates for env.js

--- frontend/assets/js/env.js
+++ frontend/assets/js/env.js
@@ -1,3 +1,8 @@
+// window.env = {
+//   API_BASE_URL: "http://localhost:8000/api/v1"
+// };
+
+
 window.env = {
   // Yahan localhost hata kar apna asli Render backend link daalein
   API_BASE_URL: "https://quickdash-front-back.onrender.com/api/v1"
