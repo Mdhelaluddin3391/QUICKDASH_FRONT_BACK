c428cb0568d963772db77c22c647e1d58860b4df
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2026-02-22 01:48:21 +0530

    some updates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -347,6 +347,13 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
